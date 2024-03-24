5fa2799abda1d0e26b8076f1c7820f2b08195dc5
Raphael <Hargenx@hotmail.com>
2024-03-24 01:43:54 -0300

    Colocar try:

---
+++ src/main/java/com/aula2024/Main.java
@@ -0,0 +1,9 @@
+package com.aula2024;
+import com.aula2024.controller.MainController;
+
+public class Main {
+    public static void main(String[] args) {
+        MainController controller = new MainController();
+        controller.run();
+    }
+}
