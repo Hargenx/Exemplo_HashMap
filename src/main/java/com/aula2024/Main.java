245eb9d81a59678f1a0b53b357a72c8df5f624c3
Raphael <Hargenx@hotmail.com>
2024-03-24 22:26:39 -0300

    Colocado

--- src/main/java/com/aula2024/Main.java
+++ src/main/java/com/aula2024/Main.java
@@ -3,7 +3,11 @@ import com.aula2024.controller.MainController;
 
 public class Main {
     public static void main(String[] args) {
-        MainController controller = new MainController();
-        controller.run();
+        try {
+            MainController controller = new MainController();
+            controller.iniciar();
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro durante a execução do programa: " + e.getMessage());
+        }
     }
 }
--- src/main/java/com/aula2024/controller/MainController.java
+++ src/main/java/com/aula2024/controller/MainController.java
@@ -22,7 +22,7 @@ public class MainController {
         hm.put(3, new Estudante(2062020, "Gilson", 6f));
     }
 
-    public void run() {
+    public void iniciar() {
         int opcao;
         do {
             opcao = view.exibirMenu();
@@ -44,60 +44,110 @@ public class MainController {
         view.closeScanner();
     }
 
-    private void exibirAlunos() {
-        if (!hm.isEmpty()) {
-            hm.forEach((key, value) -> {
-                System.out.println("Matricula: " + value.getMatricula());
-                System.out.println("Nome: " + value.getNome());
-                System.out.println("Nota: " + value.getNota());
-                System.out.println("-----------------------------");
-            });
-        } else {
-            System.out.println("Não há alunos cadastrados.");
+    private void cadastrarAluno() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Digite a Matricula: ");
+            int matricula = scanner.nextInt();
+
+            System.out.println("Digite o nome: ");
+            String nome = scanner.next();
+
+            System.out.println("Digite a nota: ");
+            float nota;
+            while (true) {
+                while (!scanner.hasNextFloat()) {
+                    System.out.println("Por favor, digite um número decimal válido para a nota: ");
+                    scanner.next(); // Limpa o buffer
+                }
+                nota = scanner.nextFloat();
+                if (nota < 0 || nota > 10) {
+                    System.out.println("A nota deve estar entre 0 e 10. Por favor, digite novamente: ");
+                } else {
+                    break;
+                }
+            }
+
+            hm.put(chave++, new Estudante(matricula, nome, nota));
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao cadastrar o aluno: " + e.getMessage());
         }
     }
 
-    private void cadastrarAluno() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite a Matricula: ");
-        int matricula = scanner.nextInt();
-        System.out.println("Digite o nome: ");
-        String nome = scanner.next();
-        System.out.println("Digite a nota: ");
-        float nota = scanner.nextFloat();
-        hm.put(chave++, new Estudante(matricula, nome, nota));
+    private void exibirAlunos() {
+        try {
+            if (!hm.isEmpty()) {
+                hm.forEach((key, value) -> {
+                    System.out.println("Matricula: " + value.getMatricula());
+                    System.out.println("Nome: " + value.getNome());
+                    System.out.println("Nota: " + value.getNota());
+                    System.out.println("-----------------------------");
+                });
+            } else {
+                System.out.println("Não há alunos cadastrados.");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao exibir os alunos: " + e.getMessage());
+        }
     }
 
     private void alterarNota() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o nome do aluno que deseja alterar a nota: ");
-        String nomePesquisa = scanner.next();
-        boolean alunoEncontrado = false;
-        for (Estudante estudante : hm.values()) {
-            if (estudante.getNome().equalsIgnoreCase(nomePesquisa)) {
-                System.out.println("Digite a nova nota: ");
-                float novaNota = scanner.nextFloat();
-                estudante.setNota(novaNota);
-                alunoEncontrado = true;
-                break;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite o nome do aluno que deseja alterar a nota: ");
+            String nomePesquisa = scanner.next();
+            boolean alunoEncontrado = false;
+            for (Estudante estudante : hm.values()) {
+                if (estudante.getNome().equalsIgnoreCase(nomePesquisa)) {
+                    float novaNota;
+                    while (true) {
+                        System.out.println("Digite a nova nota: ");
+                        while (!scanner.hasNextFloat()) {
+                            System.out.println("Por favor, digite um número decimal válido para a nota: ");
+                            scanner.next(); // Limpa o buffer
+                        }
+                        novaNota = scanner.nextFloat();
+                        if (novaNota < 0 || novaNota > 10) {
+                            System.out.println("A nota deve estar entre 0 e 10. Por favor, digite novamente: ");
+                        } else {
+                            break;
+                        }
+                    }
+                    estudante.setNota(novaNota);
+                    alunoEncontrado = true;
+                    break;
+                }
             }
-        }
-        if (!alunoEncontrado) {
-            System.out.println("Aluno não encontrado.");
+            if (!alunoEncontrado) {
+                System.out.println("Aluno não encontrado.");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao alterar a nota do aluno: " + e.getMessage());
         }
     }
 
     private void excluirAluno() {
-        Scanner scanner = new Scanner(System.in);
-        exibirAlunos();
-        System.out.println("Digite o ID do aluno que deseja excluir: ");
-        int id = scanner.nextInt();
-        Estudante alunoRemovido = hm.remove(id);
-        if (alunoRemovido != null) {
-            System.out.println("Aluno removido com sucesso:");
-            System.out.println(alunoRemovido);
-        } else {
-            System.out.println("Aluno não encontrado.");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            exibirAlunos();
+            System.out.println("Digite o ID do aluno que deseja excluir: ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Por favor, digite um número inteiro válido para o ID do aluno: ");
+                scanner.next(); // Limpa o buffer
+            }
+            int id = scanner.nextInt();
+            Estudante alunoRemovido = hm.remove(id);
+            if (alunoRemovido != null) {
+                System.out.println("Aluno removido com sucesso:");
+                System.out.println("Matricula: " + alunoRemovido.getMatricula());
+                System.out.println("Nome: " + alunoRemovido.getNome());
+                System.out.println("Nota: " + alunoRemovido.getNota());
+            } else {
+                System.out.println("Aluno não encontrado.");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao excluir o aluno: " + e.getMessage());
         }
     }
+
 }
